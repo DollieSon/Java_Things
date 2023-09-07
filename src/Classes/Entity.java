70bb12b188f5ccdbbd7108e93c440218b62c9c5b
Lab <rayanthony.dollison@cit.edu>
2023-09-07 17:04:47 +0800

    Added more clarification on attacks

--- src/Classes/Entity.java
+++ src/Classes/Entity.java
@@ -1,6 +1,51 @@
 package Classes;
 
 public class Entity {
-    public int age = 0;
+    private String Name;
+    private int MaxHealth;
+    private int CurrHealth;
+    private int Damage;
+
+    public Entity(String name, int maxHealth, int damage) {
+        Name = name;
+        MaxHealth = maxHealth;
+        CurrHealth = maxHealth;
+        Damage = damage;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public int getMaxHealth() {
+        return MaxHealth;
+    }
+
+    public int getCurrHealth() {
+        return CurrHealth;
+    }
+    public int getDamage() {
+        return Damage;
+    }
+
+    public int GetAttacked(int damage){
+        CurrHealth -= damage;
+        if(CurrHealth < 0){
+            return -1;
+        }
+        return 0;
+    }
+    public boolean IsAlive(){
+        return (CurrHealth > 0);
+    }
+
+    public void ResetHealth(){
+        CurrHealth = MaxHealth;
+    }
+
+    public void victory(){
+        System.out.println(this.Name + " Won!");
+    }
+
 
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
+import Classes.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Entity mc = new Entity("Person",10,2);
+        Entity enemy = new Entity("Goblin", 5, 1);
+        while(mc.IsAlive() && enemy.IsAlive()){
+            mc.GetAttacked(enemy.getDamage());
+            System.out.println(enemy.getName() + " Attacks " + mc.getName() + " For "+ enemy.getDamage() + " Damage");
+            enemy.GetAttacked(mc.getDamage());
+            System.out.println(mc.getName() + " Attacks " + enemy.getName() + " For "+ mc.getDamage() + " Damage ");
+        }
+        System.out.println("  ");
+        if(mc.IsAlive()){
+            mc.victory();
+        }else{
+            enemy.victory();
+        }
     }
+
 }
